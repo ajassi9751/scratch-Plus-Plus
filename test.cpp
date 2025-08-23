cb48a1d67faa3a49f2635e911fecac524a0258c8
ajassi9751 <ajassi9751@gmail.com>
2025-08-23 17:25:40 -0400

    Actually got a window to open

--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<thread>
-#include<GL/glut.h>
+#include<GLFW/glfw3.h>
 
 // This file is used for testing
 
@@ -9,24 +9,61 @@ void thrdinit () {
 	std::cout << "created a thread\n";
 }
 
-int main (int argc, char **argv) {
-	glutInit(&argc, argv);
+void windowRenderer (GLFWwindow* window) {
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window)) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
+void framebuffer_size_callback (GLFWwindow* window, int width, int height) {
+	glViewport(0,0,width,height);
+}
+
+int main () {
+	//Initialization
+	if (!glfwInit()) {
+		std::cerr << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Scratch", nullptr, nullptr);
+	if (!window) {
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
 	unsigned int threadsnum = std::thread::hardware_concurrency();
+	unsigned int threadAvail = std::thread::hardware_concurrency();
+
 	std::thread threads[threadsnum];
-	for (unsigned int i = 0; i < threadsnum; i++) {
-		threads[i] = std::thread(thrdinit);
+
+	threads[0] = std::thread(windowRenderer, window); //The render thread
+	threadAvail--;
+	
+	//for (unsigned int i = 1; i < threadsnum; i++) {
+		//threads[i] = std::thread(thrdinit);
+	//}
+	//for (unsigned int i = 1; i < threadsnum; i++) {
+		//if (threads[i].joinable())
+			//threads[i].join();
+	//}
+	//std::cout << "Initialized " << threadsnum << " threads";
+	//while (true) {
+		//if (threads[0].joinable())
+			//threads[0].join();
+	//}
+	while (!glfwWindowShouldClose(window)) {
+		//Wait idk
 	}
-	for (unsigned int i = 0; i < threadsnum; i++) {
-		if (threads[i].joinable())
-			threads[i].join();
-	}
-	for (unsigned int i = 0; i < threadsnum; i++) {
-		threads[i] = std::thread(thrdinit);
-	}
-	for (unsigned int i = 0; i < threadsnum; i++) {
-		if (threads[i].joinable())
-			threads[i].join();
-	}
-	std::cout << "Initialized " << threadsnum << " threads";
+	if (threads[0].joinable())
+		threads[0].join();
+
+	glfwTerminate();
 	return 0;
 }
