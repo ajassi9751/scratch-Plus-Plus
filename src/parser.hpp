0c1d974f8587fb9941f2d84321865b524afbbf36
ajassi9751 <ajassi9751@gmail.com>
2025-09-10 15:51:34 +0000

    Added some things to project and parser

--- src/parser.hpp
+++ src/parser.hpp
@@ -1,8 +1,38 @@
 // This file parses a json file and creates a data structure out of it
 #include<stdio.h>
-#include<string.h>
+#include<cstring>
 
-void parsejson(const char path[]) {
+/*
+@brief Finds a substring in a string and returns its position in said string
+@param str The main string
+@param substr The substring
+@param instance Which occurence of the substring you want to find (the first time it appears, the second time it appears ect)
+@return Returns an int of the position of the substring in the string, returns -1 if not found
+*/
+int findInStr (const char* str, const char substr[], int instance) {
+    bool isTrue = false;
+    int count = 0;
+    for (int i = 0; i < strlen(str); ++i) {
+        if (str[i] == substr[0]) {
+            for (int a = 1; a < strlen(substr); ++a) {
+                if (str[i+a] == substr[a]) {
+                    isTrue = true;
+                }
+                else {
+                    isTrue = false;
+                    break;
+                }
+            }
+            count++;
+            if (isTrue && count == instance) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+void parsejson(const char path[]) { //Could use the c++ style of doing this tho
     FILE *fptr = fopen(path, "r");
     fseek(fptr, 0, SEEK_END);
     long length = ftell(fptr);
@@ -10,4 +40,7 @@ void parsejson(const char path[]) {
     char fbuff[length];
     fgets(fbuff,length,fptr);
     // Creates a string with the contents of the whole file, this may be unviable for large files
+    if (findInStr(fbuff, "variables", 1) != -1) {
+        int flag = findInStr(fbuff, "variables", 1);
+    }
 }
--- src/project.hpp
+++ src/project.hpp
@@ -2,28 +2,78 @@
 #include<vector>
 #include<string>
 
-class script {
+const std::string events[] = {"eventWhenFlagClicked"}; //Stores valid strings for events
+
+template<typename T>
+class eventStarter { // Used for things like eventWhenFlagClicked probably should be in parser.hpp
 	public:
-		std::vector<std::string> scripts;
+		std::string event;
+		T* nxtptr = nullptr;
+};
+
+template<typename T>
+class stdChnk { // Used to store a block of code from the scratch "standard library" (maybe I should make a standard library for scratch)
+	public:
+		// Not sure if this is needed because everything is basically a function call or a varible setting
+		std::string operator;
+		T* nxtptr = nullptr;
+};
+
+template<typename T, int S>
+class funcCall { // Calls a function
+	public:
+		std::string funcName;
+		express args[S];
+		double return1;
+		bool return2; // Maybe not needed because scratch doesnt really have bools, they are either represented as 1, 0, or a string
+		std::string return3;
+		bool hasReturn; // Need to have a way to store return types and values
+		T* nxtptr = nullptr;
+};
+
+template<typename T>
+class express { // Used to store expressions inside of while loops or whatever else
+	public:
+		// Expressions will most likley be "copy pasted" into code
+		char op; // Used for + - / * etc
+		char op2; // Optionally used for logical or boolean operators like == && ||
+		funcCall func; // Used if a value is a function
+		funcCall func2; // Also very recursive stuff going on here
+		std::string varid1; // Used if there is a varible in an expression
+		std::string varid2;
+		express operand1; // Used for nestled expressions
+		express operand2; // Maybe make these pointers
+		bool isBool; // Not sure if this is needed
+};
+
+class varSet { // Stores variables for a sprite or globaly
+	public:
+		std::vector<std::string> vars;
+		std::vector<std::string> varids;
+};
+
+class globalObjects { // Used to store global variables and functions and stuff
+	public:
+		varSet globalVars;
 };
 
 class sprite {
 	private:
 		bool isshown;
-		std::vector<script> scripts;
+		//std::vector<script> scripts;
 		std::vector<std::string> costumes; //Contains the paths to the costumes
-		std::vector<std::string> vars;
+		varSet vars;
 		int costumeshown;
 		void reload_appearance () {
-			//Switches the costume to the current costume
+			//Switches the costume to the current costume and calls some kind of renderer function
 		}
 	public:
 		std::string get_costume (int index) {
-			return costume[index];
+			return costumes[index];
 		}
-		std::string get_var (int index) {
-			return vars[index];
-		}
+		//std::string get_var (int index) {
+			//return vars[index];
+		//}
 		std::string get_current_costume () {
 			return costumes[costumeshown];
 		}
@@ -34,9 +84,9 @@ class sprite {
 		void add_costume (std::string tstring) {
 			costumes.push_back(tstring);
 		}
-		void add_var (std::string tstring) {
-			vars.push_back(tstring);
-		}
+		//void add_var (std::string tstring) {
+			//vars.push_back(tstring);
+		//}
 		void change_shown (bool shown) {
 			isshown = shown;
 			reload_appearance();
@@ -51,8 +101,8 @@ class backdrop {
 			
 		}
 	public:
-		void switch_backdrop (backdrop) {
-			backdropshown = backdrop;
+		void switch_backdrop (int bckdrp) {
+			backdropshown = bckdrp;
 			reload_backdrop();
 		}
 		void add_backdrop (std::string tstring) {
@@ -68,7 +118,8 @@ class backdrop {
 
 class project {
     public:
-	std::vector<std::string> globalVars;
-        std::vector<sprite> sprites;
-        backdrop background;
+		std::vector<std::string> globalVars;
+    	std::vector<sprite> sprites;
+    	backdrop background;
+		globalObjects globals;
 };
